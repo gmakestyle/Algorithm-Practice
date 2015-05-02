94304cda1edfb546372b040f45d8ee47585c85cd
Maajid <maajid.nazrulla@gmail.com>
2015-05-01 21:58:46 -0700

    Add problem description to rats.py

--- hackerrank/contests/Apr2015/rats.py
+++ hackerrank/contests/Apr2015/rats.py
@@ -1,3 +1,12 @@
+"""
+This is a solution to the first problem of the April 2015 Hacker Rank contest.
+The problem states that there are N rats that each begin a race at different
+distances from a finish line. They each also begin with a constant speed. The
+solution determines which rats finish first. The rats are identified by their 
+respective positions in the input list (1-indexed). The output format is simply
+a printout of the index of each winning rat on a separate line, sorted from 
+least index to greatest.
+"""
 numRats = int(raw_input())
 ratSpeeds = [int(s) for s in raw_input().split()]
 ratDistances = [int(s) for s in raw_input().split()]
